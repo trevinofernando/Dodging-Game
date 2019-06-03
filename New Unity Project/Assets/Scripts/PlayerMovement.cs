6bf138ed6f14dfa463b7299187d427e80ded7ad0
trevinofernando <trevino.96@hotmail.com>
2019-06-03 14:02:53 -0400

    Initial commit with functional game

---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float fowardForce = 1000f;
+    public float sideForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Starts...");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() // "FixUpdate" since we are manipulating physics
+    {
+        //rb.AddForce(0, 0, fowardForce * Time.deltaTime); //deltatime is number of seconds since last frame
+
+        if( Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/objectPooler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectPooler : MonoBehaviour
+{
+    [System.Serializable] //This will make it appear on Unity
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+
+    public static objectPooler Instace;
+
+    private void Awake()
+    {
+        Instace = this;
+    }
+
+    #endregion
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool pool in pools) //For each pool of objects
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for(int i = 0; i < pool.size; i++) //loop thru all objects in the pool
+            {
+                GameObject obj = Instantiate(pool.prefab); //create the object according to the given prefab
+                obj.SetActive(false);  //disable it so it's not visible just yet
+                objectPool.Enqueue(obj); //add it to the queue
+            }
+
+            poolDictionary.Add(pool.tag, objectPool); //add queue to the dictionary pf pools
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't exist.");
+            return null;
+        }
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+        return objectToSpawn;
+    }
+
+}
